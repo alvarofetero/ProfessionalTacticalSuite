afc00526516eda12752efbd0af59033f454cc11b
Alvaro Fernández <alvarofetero@gmail.com>
2026-06-28 14:37:28 +0200

    reduce App.test.jsx to only test the integration

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,19 +1,25 @@
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
+import { render, screen, fireEvent } from '@testing-library/react'
+import {describe, it, expect} from 'vitest'
 import App from './App'
 
-describe('App layout', () => {
-   it('renders the top bar with the app title', () => {
-     render(<App />)
-     expect(screen.getByText(/Video Drawing Studio HD/i)).toBeInTheDocument()
-   })
 
-  // it('renders the main video workspace', () => {
-  //   render(<App />)
-  //   expect(screen.getByRole('region', { name: /video workspace/i })).toBeInTheDocument()
-  // })
+describe('App Integration', ()=> {
 
+  it('should change the active tool when a button is clicked on the Sidebar', () => {
+      render(<App />);
+      
+      // 1. Search for a tool button on the Sidebar
+      const arrowButton = screen.getByText('Arrow');
+      
+      // 2. Simulate a click
+      fireEvent.click(arrowButton);
+      
+      // 3. Verify  that the change is reflcted (ex.: button styl changed)
+      // Now the button has the class "active"
+      expect(arrowButton.className).toContain('bg-sky-500');
+    });
+
+});
  
 
-  
-})
+
