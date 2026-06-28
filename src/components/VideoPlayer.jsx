d5e85acb6b82568e8ec7b77652113827937aad34
Alvaro Fernández <alvarofetero@gmail.com>
2026-06-28 19:01:27 +0200

    change style to get the input type from the tests as well

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -40,7 +40,7 @@ function VideoPlayer({ onTimeUpdate, onDurationChange, onPlayStateChange }, ref)
         onTimeUpdate={() => onTimeUpdate?.(videoRef.current?.currentTime || 0)}
         onDurationChange={() => onDurationChange?.(videoRef.current?.duration || 0)}
       />
-      <input ref={inputRef} type="file" accept="video/*" className="sr-only" onChange={handleFileChange} />
+      <input ref={inputRef} type="file" accept="video/*" style={{ display: 'none'}} onChange={handleFileChange} />
       
       {!videoLoaded && (
         <div className="absolute inset-0 flex items-center justify-center bg-slate-950/90 text-slate-400 text-sm font-medium">
